8cd1345fb8b29495023cfd2c3164929f8d2814c3
Damo <daiguimao@sightp.com>
2020-06-28 19:31:54 +0800

    tmp swipe

--- pages/stage-animation/stage-animation.js
+++ pages/stage-animation/stage-animation.js
@@ -1,4 +1,23 @@
 // pages/movable/movable.js
+
+const POI_1_STAGE = {
+  HIDDEN: 0,
+  BOTTOM: 1,
+  TOP: 2,
+}
+
+const POI_1_BOTTOM = {
+  [POI_1_STAGE.HIDDEN]: '-1050rpx',
+  [POI_1_STAGE.BOTTOM]: '-300rpx',
+  [POI_1_STAGE.TOP]: '0rpx',
+}
+
+const EVENT = {
+  INIT: 'init',
+  BEFORE_STAGE_CHANGED: 'beforeStageChanged',
+  STAGE_CHANGED: 'stageChanged',
+}
+
 Page({
 
   /**
@@ -9,9 +28,12 @@ Page({
     top: 50,
     taptest: 'taptest',
     show: true,
-    dataObj: {
-      obj: 1
-    }
+    event: { // 'init','beforeStageChanged','stageChanged'
+      type: 'init',
+      px2rpx: 750 / wx.getSystemInfoSync().windowWidth,
+      stages: ['-1050rpx', '-200rpx', '0rpx'], // stage:0,1,2,...
+      currentStage: 0,
+    },
   },
 
   /**
@@ -36,7 +58,36 @@ Page({
       show: !this.data.show,
     })
   },
-  animationToStage() {
-
-  }
+  animationToStage(e) {
+    console.log('animationToStage', e)
+  },
+  poiInfoTouchStart(e) {
+    console.log('poiInfoTouchStart', e)
+  },
+  poiInfoTouchEnd(e) {
+    console.log('poiInfoTouchEnd', e)
+  },
+  toStage(stage) {
+    const bottom = POI_1_BOTTOM[stage];
+    const animation = wx.createAnimation({
+      duration: 600,
+      timingFunction: 'ease-in-out',
+    }).bottom(bottom).step();
+    this.setData({
+      animation: animation.export(),
+      event: {
+        type: EVENT.BEFORE_STAGE_CHANGED,
+        nextStage: stage
+      }
+    });
+  },
+  stage0() {
+    this.toStage(POI_1_STAGE.HIDDEN);
+  },
+  stage1() {
+    this.toStage(POI_1_STAGE.BOTTOM);
+  },
+  stage2() {
+    this.toStage(POI_1_STAGE.TOP);
+  },
 })
